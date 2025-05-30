fcf5f2b1acd4c678742419b9d49f1997bb574cb8
rrk18 <rajeevrkr07@gmail.com>
2025-05-30 16:15:23 +0530

    added readingmaterial

--- python/basic.py
+++ python/basic.py
@@ -4,3 +4,13 @@ def add(*args):
 
 print(add(34,12,56,78,89,12,34))
 
+# def division():
+#     try:
+#         num1=float(input("Enter first number: "))
+#         num2=float(input("Enter 2nd number: "))
+#         result=num1/num2
+#     except TypeError:
+#         print(f"You have not entered int or float value {TypeError}")
+#     except ZeroDivisionError:
+#         print(f"Any number")
+
